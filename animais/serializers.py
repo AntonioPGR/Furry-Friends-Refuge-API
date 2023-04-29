818f656862f3c511745549f255650f7006f34dec
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-04-29 20:12:40 -0300

    FEAT: add read only files to serializers

--- animais/serializers.py
+++ animais/serializers.py
@@ -5,17 +5,23 @@ class EspeciesSerializer(ModelSerializer):
   class Meta:
     model = Especie
     fields = '__all__'
+    read_only_fields = ['id']
 
   
 class RacasSerializer(ModelSerializer):
   class Meta:
     model = Raca
     fields = '__all__'
+    depth = 1
+    read_only_fields = ['id']
 
   
 class AnimaisSerializer(ModelSerializer):
   class Meta:
     model = Animal
     fields = '__all__'
+    depth = 1
+    read_only_fields = ['id']
+    
 
   
--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -5,3 +5,4 @@ class UsuariosSerializer(ModelSerializer):
   class Meta:
     model = Usuario
     fields = '__all__'
+    read_only_fields = ['id']
